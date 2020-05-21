36e71820963bf1b103433cf758238f79f9a36e8c
lirm <1409927368@qq.com>
2020-05-21 17:36:02 +0800

    0.2 init and orm

--- handler/route.go
+++ handler/route.go
@@ -1,16 +1,15 @@
 package handler
 
-//
-//func r()  {
-//    r := gin.Default()
-//    v1 := r.Group("/api/v1/todo")
-//    {
-//        v1.POST("/", add)      // 添加新条目
-//        v1.GET("/", all)       // 查询所有条目
-//        v1.GET("/:id", take)   // 获取单个条目
-//        v1.PUT("/:id", update) // 更新单个条目
-//        v1.DELETE("/:id", del) // 删除单个条目
-//    }
-//    r.Run(":9089")
-//
-//}
+func r() {
+	//r := gin.Default()
+	//v1 := r.Group("/api/v1/todo")
+	//{
+	//    v1.POST("/", add)      // 添加新条目
+	//    v1.GET("/", all)       // 查询所有条目
+	//    v1.GET("/:id", take)   // 获取单个条目
+	//    v1.PUT("/:id", update) // 更新单个条目
+	//    v1.DELETE("/:id", del) // 删除单个条目
+	//}
+	//r.Run(":9089")
+
+}
